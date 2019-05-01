77f71e826fa4c6c497c3784e33468bea3e1ce19c
NicoleBruce <ZTheCrazed@Gmail.com>
2019-05-01 13:42:06 -0400

    Added functionality for user thread libraries (thread_init,thread_acquire,thread_release,thread_create, and thread_join)

--- user/ulib.c
+++ user/ulib.c
@@ -4,6 +4,49 @@
 #include "user.h"
 #include "x86.h"
 
+//new user libraries for lock functions
+//for lock_t typedef, see user.h
+int lock_init(lock_t *lk){
+  lk->flag = 0;
+  return 0;
+}
+
+int lock_acquire(lock_t *lk){
+  while(xchg(&lk->flag,1) != 0)
+    ; //keep spinning
+}
+
+int lock_release(lock_t *lk){
+  xchg($lk->flag,0);
+}
+
+int thread_create(void (*start_routine)(void*),void *arg){
+  lock_t lk;
+  lock_init(&lk);
+  lock_acquire(&lk);
+  void *stack = malloc(PGSIZE*2);
+  lock_release(&lk);
+
+  if((uint)stack % PGSIZE){
+    stack = stack + (PGSIZE - (uint)stack % PGSIZE);
+  }
+
+  int result = clone(start_routine,arg,stack);
+  return result;
+}
+
+int thread_join(){
+  void *stack = malloc(sizeof(void*));
+  int result = join(&stack);
+  lock_t lk;
+  lock_init(&lk);
+  lock_acquire(&lk);
+  free(stack);
+  lock_release(&lk);
+  return result;
+}
+//end new functions
+
 char*
 strcpy(char *s, char *t)
 {
