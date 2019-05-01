e94cad2ebebc28ee56d13fcb6cb946e7034fa7b0
NicoleBruce <ZTheCrazed@Gmail.com>
2019-05-01 19:18:13 -0400

    Added new member for proc member list (tstack)

--- kernel/proc.h
+++ kernel/proc.h
@@ -74,6 +74,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  uint tstack;                 //ADDED FOR MINI PROJ 4--thread stack address
 };
 
 // Process memory is laid out contiguously, low addresses first:
